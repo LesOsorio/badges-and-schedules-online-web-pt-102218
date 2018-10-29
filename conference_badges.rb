8634f0ba03721e23509fd638b938ec71e4a3e41f
Leslie Osorio <lesosorio17@gmail.com>
2018-10-29 19:37:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,6 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+end 
